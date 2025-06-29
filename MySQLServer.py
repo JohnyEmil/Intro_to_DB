d92c98bb0fcf304d2d28d75201db071a3a888f2f
JohnyEmil <jonyjony7030@gmail.com>
2025-06-29 23:14:24 +0300

    second task

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    # الاتصال بخادم MySQL
+    connection = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='johny'
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+
+        # إنشاء قاعدة البيانات إذا لم تكن موجودة
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"❌ Error while connecting to MySQL: {e}")
+
+finally:
+    # إغلاق الاتصال
+    if 'connection' in locals() and connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed.")
